81114839d3635331a4344941f6effbd8584f21a9
Kristina <EgorovaKrista58@yandex.ru>
2025-04-07 16:04:14 +0300

    Изменила код

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -3,11 +3,20 @@
 
 int main()
 {
-	int c = 0;
-    int a1 = 5, b = 2, y;
-    y = a + b;
-    printf("5+5 = %d", y);
-	printf("c = %d", y+b+a1);
+  int arr[10] = { 5, -3, 0, 7, -8, 12, -1, 4, -6, 9 };
+  int a1 = 5; //желаемая оценка
+  int b = 2;  //НЕжелаемая оценка
+  int y;
+  y = a1 + b;
+  printf("a = %d", a1);
+  printf("\nb = %d", b);
+  printf("\na + b = %d", y);
 
+  printf("\nОтрицательные числа в массиве:");
+  for (int i = 0; i < 10; i++) {
+    if (arr[i] < 0) {
+      printf("%d ", arr[i]);
+    }
+  }
+  printf("\n a + b + y = %d", a1+b+y);
 }
-
